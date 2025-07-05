ae9abf37828413c58de4c25540cf3ba70be28492
Kanyapat-samee <kanyapat.samee@gmail.com>
2025-07-06 05:54:22 +0700

    initial commit

--- src/lib/aws.ts
+++ src/lib/aws.ts
@@ -4,8 +4,8 @@ import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb'
 const client = new DynamoDBClient({
   region: 'ap-southeast-1',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.ACCESS_KEY_ID!,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY!,
   },
 })
 
