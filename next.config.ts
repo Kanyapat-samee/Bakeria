5c7890780d2a76db15b7a88f80caaafe9ab4f015
Kanyapat-samee <kanyapat.samee@gmail.com>
2025-07-06 09:56:00 +0700

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
-};
-
-module.exports = {
   images: {
-    domains: ['bakeria-cakes.s3.ap-southeast-1.amazonaws.com',
-    'bakeria-content-picture.s3.ap-southeast-1.amazonaws.com',],
+    domains: [
+      'bakeria-cakes.s3.ap-southeast-1.amazonaws.com',
+      'bakeria-content-picture.s3.ap-southeast-1.amazonaws.com',
+    ],
   },
-}
+  env: {
+    ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
+    SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
+  },
+};
 
 export default nextConfig;
--- src/lib/aws.ts
+++ src/lib/aws.ts
@@ -1,15 +1,19 @@
-// ใช้แบบนี้สำหรับ Server (เช่น API route, getServerSideProps)
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb'
 
 const REGION = 'ap-southeast-1'
 
-const client = new DynamoDBClient({
-  region: REGION,
-  credentials: {
-    accessKeyId: process.env.ACCESS_KEY_ID!,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY!,
-  },
-})
+if (!process.env.ACCESS_KEY_ID || !process.env.SECRET_ACCESS_KEY) {
+  console.error('❌ Missing AWS credentials in ENV')
+  throw new Error('Missing AWS credentials')
+}
 
-export const db = DynamoDBDocumentClient.from(client)
+export const db = DynamoDBDocumentClient.from(
+  new DynamoDBClient({
+    region: REGION,
+    credentials: {
+      accessKeyId: process.env.ACCESS_KEY_ID,
+      secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    },
+  })
+)
