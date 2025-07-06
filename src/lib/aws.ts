7836caec801c5071f845fed68bfd19bdced7cbac
Kanyapat-samee <kanyapat.samee@gmail.com>
2025-07-06 09:20:40 +0700

    initial commit

--- src/lib/aws.ts
+++ src/lib/aws.ts
@@ -1,12 +1,23 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb'
+import { fetchAuthSession } from 'aws-amplify/auth'
 
-const client = new DynamoDBClient({
-  region: 'ap-southeast-1',
-  credentials: {
-    accessKeyId: process.env.ACCESS_KEY_ID!,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY!,
-  },
-})
+const REGION = 'ap-southeast-1'
 
-export const db = DynamoDBDocumentClient.from(client)
+export async function getDynamoDbDocClient(): Promise<DynamoDBDocumentClient> {
+  const session = await fetchAuthSession()
+  const creds = session.credentials
+
+  if (!creds) throw new Error('❌ Missing AWS credentials')
+
+  const client = new DynamoDBClient({
+    region: REGION,
+    credentials: {
+      accessKeyId: creds.accessKeyId,
+      secretAccessKey: creds.secretAccessKey,
+      sessionToken: creds.sessionToken, // สำคัญมากถ้าใช้ temporary credentials
+    },
+  })
+
+  return DynamoDBDocumentClient.from(client)
+}
